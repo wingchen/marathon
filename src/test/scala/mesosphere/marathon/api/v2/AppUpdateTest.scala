5a7906492111eb1edc230ad6af0d293e38e461b3
Connor Doyle <connor@mesosphere.io>
2014-03-20 15:12:21 -0700

    Added test for AppUpdate deserialization

--- src/test/scala/mesosphere/marathon/api/v2/AppUpdateTest.scala
+++ src/test/scala/mesosphere/marathon/api/v2/AppUpdateTest.scala
@@ -33,4 +33,24 @@ class AppUpdateTest {
 
   }
 
+  @Test
+  def testDefaultValues() {
+    import com.fasterxml.jackson.databind.ObjectMapper
+    import com.fasterxml.jackson.module.scala.DefaultScalaModule
+    import mesosphere.marathon.api.v2.json.MarathonModule
+
+    val mapper = new ObjectMapper
+    mapper.registerModule(DefaultScalaModule)
+    mapper.registerModule(new MarathonModule)
+
+    val json = """{ "instances": 3 }"""
+
+    for (i <- (0 until 1024 * 1024)) {
+      val readResult = mapper.readValue(json, classOf[AppUpdate])
+      assertTrue(readResult.instances == Some(3))
+      assertTrue(readResult.uris == None)
+    }
+
+  }
+
 }
