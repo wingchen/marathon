9ed3bef5a4b7372e9364e61f54cbd448e8c51a0a
Connor Doyle <connor@mesosphere.io>
2014-03-20 15:11:23 -0700

    Added default constructor to ContainerInfo.

--- src/main/scala/mesosphere/marathon/ContainerInfo.scala
+++ src/main/scala/mesosphere/marathon/ContainerInfo.scala
@@ -5,6 +5,11 @@ import scala.collection.JavaConverters._
 
 
 case class ContainerInfo(image: String = "", options: Seq[String] = Seq()) {
+
+  // the default constructor exists solely for interop with automatic
+  // (de)serializers
+  def this() = this(image = "")
+
   def toProto: Protos.ContainerInfo =
     Protos.ContainerInfo.newBuilder()
       .setImage(ByteString.copyFromUtf8(image))
