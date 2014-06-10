024fe2a728caf5d12f73edec41694fd6417a6b7e
Matthias Veit <matthias_veit@yahoo.de>
2014-06-10 09:42:37 +0200

    Tests: import postFix feature, to vanish compiler warning

--- src/test/scala/mesosphere/marathon/state/MarathonStoreTest.scala
+++ src/test/scala/mesosphere/marathon/state/MarathonStoreTest.scala
@@ -5,12 +5,11 @@ import org.mockito.Mockito._
 import org.mockito.Matchers._
 import org.apache.mesos.state.{ InMemoryState, Variable, State }
 import java.util.concurrent.{ Future => JFuture, ExecutionException }
-import java.util.{ Iterator => JIterator }
 import java.lang.{ Boolean => JBoolean }
 import scala.concurrent.{ Future, Await }
 import scala.concurrent.duration._
 import mesosphere.marathon.{ MarathonSpec, StorageException }
-import scala.collection.JavaConverters._
+import scala.language.postfixOps
 
 class MarathonStoreTest extends MarathonSpec {
   test("Fetch") {
